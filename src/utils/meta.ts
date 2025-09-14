d6c4de054367bc266a10c26874cd4e4ee5eeb84f
Memolia <ultimum.memolia@gmail.com>
2025-09-14 13:27:29 +0900

    favicon設定

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -52,6 +52,15 @@ export const generateMetadata = (locale: string, options: PageMetaOptions = {}):
         [LOCALES.JAPANESE]: `/${LOCALES.JAPANESE}`,
       },
     },
+    icons: {
+      icon: [
+        { url: "/favicon.ico", sizes: "any" },
+        { url: "/icon.png", type: "image/png", sizes: "32x32" },
+        { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
+        { url: "/icon-512.png", type: "image/png", sizes: "512x512" },
+      ],
+      apple: [{ url: "/apple-icon.png", sizes: "180x180" }],
+    },
     openGraph: {
       title,
       description,
